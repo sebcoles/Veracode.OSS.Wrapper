733e380d4eab0536ea2f679b7426ff6f4f9e7aa6
scoles <scoles@veracode.com>
2020-10-12 15:13:34 +0100

    Added converter for build status

--- VeracodeServicesCore/VeracodeService/VeracodeEnumConverter.cs
+++ VeracodeServicesCore/VeracodeService/VeracodeEnumConverter.cs
@@ -79,5 +79,67 @@ namespace VeracodeService
             }
             return parsed_role;
         }
+
+        public static string Convert(BuildStatusType status)
+        {
+            string parsed_status;
+            switch (status)
+            {
+                case BuildStatusType.Incomplete:
+                    parsed_status = "Incomplete";
+                    break;
+                case BuildStatusType.NotSubmittedtoEngine:
+                    parsed_status = "NotSubmittedtoEngine";
+                    break;
+                case BuildStatusType.SubmittedtoEngine:
+                    parsed_status = "SubmittedtoEngine";
+                    break;
+                case BuildStatusType.ScanErrors:
+                    parsed_status = "ScanErrors";
+                    break;
+                case BuildStatusType.ScanInProcess:
+                    parsed_status = "ScanInProcess";
+                    break;
+                case BuildStatusType.ScanCancelled:
+                    parsed_status = "ScanCancelled";
+                    break;
+                case BuildStatusType.ScanInternalError:
+                    parsed_status = "ScanInternalError";
+                    break;
+                case BuildStatusType.PendingInternalReview:
+                    parsed_status = "PendingInternalReview";
+                    break;
+                case BuildStatusType.ResultsReady:
+                    parsed_status = "ResultsReady";
+                    break;
+                case BuildStatusType.PreScanSubmitted:
+                    parsed_status = "PreScanSubmitted";
+                    break;
+                case BuildStatusType.PreScanFailed:
+                    parsed_status = "PreScanFailed";
+                    break;
+                case BuildStatusType.PreScanSuccess:
+                    parsed_status = "PreScanSuccess";
+                    break;
+                case BuildStatusType.NoModulesDefined:
+                    parsed_status = "NoModulesDefined";
+                    break;
+                case BuildStatusType.PendingVendorConfirmation:
+                    parsed_status = "PendingVendorConfirmation";
+                    break;
+                case BuildStatusType.VendorReviewing:
+                    parsed_status = "VendorReviewing";
+                    break;
+                case BuildStatusType.PreScanCancelled:
+                    parsed_status = "PreScanCancelled";
+                    break;
+                case BuildStatusType.ScanOnHold:
+                    parsed_status = "ScanOnHold";
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("Build status can only be between 0 - 16");
+            }
+            return parsed_status;
+        }
     }
 }
