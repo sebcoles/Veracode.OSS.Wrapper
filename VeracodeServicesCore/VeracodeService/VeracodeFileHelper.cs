f0bd757548b83ea61c314b3f2a68c37ff798095f
scoles <scoles@veracode.com>
2020-10-12 13:24:49 +0100

    Added a helper for taking veracode credentials from the .credential file

---
+++ VeracodeServicesCore/VeracodeService/VeracodeFileHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using VeracodeService.Configuration;
+
+namespace VeracodeService
+{
+    public static class VeracodeFileHelper
+    {
+        public static VeracodeConfiguration GetConfiguration(string filelocation)
+        {
+            string apikey = "", apiId = "";
+            var filePath = Environment.ExpandEnvironmentVariables(filelocation);
+            using (var file = new System.IO.StreamReader(filePath))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (line.Contains("veracode_api_key_id"))
+                        apiId = line.Replace(" ", "").Substring(20);
+
+                    if (line.Contains("veracode_api_key_secret "))
+                        apikey = line.Replace(" ", "").Substring(24);
+                }
+            }
+
+            return new VeracodeConfiguration
+            {
+                ApiId = apiId,
+                ApiKey = apikey
+            };
+        }
+    }
+}
