487d52aad5c32feda0a7f3b642e56a57b97570fc
scoles <scoles@veracode.com>
2020-11-08 12:56:06 +0000

    Adding environmen variable helper class

---
+++ Veracode.OSS.Wrapper.Tests/Integration/VeracodeEnvHelperTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Veracode.OSS.Wrapper.Tests.Integration
+{
+    [TestFixture]
+    public class VeracodeEnvHelperTests
+    {
+        private string apiIdLabel = "VERACODE_API_KEY_ID";
+        private string apiKeyLabel = "VERACODE_API_KEY_SECRET";
+        private string test_id = "jam_id";
+        private string test_key = "jam_key";
+        [SetUp]
+        public void SetEnvVariables()
+        {
+            Environment.SetEnvironmentVariable(apiIdLabel, test_id);
+            Environment.SetEnvironmentVariable(apiKeyLabel, test_key);
+        }
+
+        [Test]
+        public void VeracodeConfiguration_GetCredentials()
+        {
+            var configuration = VeracodeEnvHelper.GetConfiguration();
+            Assert.AreEqual(test_id, configuration.ApiId);
+            Assert.AreEqual(test_key, configuration.ApiKey);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Environment.SetEnvironmentVariable(apiIdLabel, null);
+            Environment.SetEnvironmentVariable(apiKeyLabel, null);
+        }
+    }
+}
---
+++ Veracode.OSS.Wrapper/VeracodeEnvHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Veracode.OSS.Wrapper.Configuration;
+using Salaros.Configuration;
+
+namespace Veracode.OSS.Wrapper
+{
+    public static class VeracodeEnvHelper
+    {
+        public static VeracodeConfiguration GetConfiguration(string api_id_env_name = "VERACODE_API_KEY_ID", string api_key_env_name = "VERACODE_API_KEY_SECRET")
+        {
+            string apikey = Environment.GetEnvironmentVariable(api_key_env_name);
+            string apiId = Environment.GetEnvironmentVariable(api_id_env_name);
+
+            if (string.IsNullOrWhiteSpace(apiId))
+                throw new ArgumentException("Value retrieved for veracode_api_key_id from ENV variables is empty!");
+
+            if (string.IsNullOrWhiteSpace(apikey))
+                throw new ArgumentException("Value retrieved for veracode_api_key_secret from ENV variables is empty!");
+
+            return new VeracodeConfiguration
+            {
+                ApiId = apiId,
+                ApiKey = apikey
+            };
+        }
+    }
+}
